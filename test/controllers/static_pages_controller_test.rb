748244dedd759a4e1cba049d45baebd9f0319f9e
Dominic Meddick <dominicmeddick@gmail.com>
2022-01-05 10:59:43 +0000

    Contact page set up with passing test. TDD complete

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,25 +2,27 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Chitter"
-  end
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "Home | Chitter"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "Help | Chitter"
   end
 
-  test 'should get about' do
+  test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "About | Chitter"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Chitter"
   end
 end
