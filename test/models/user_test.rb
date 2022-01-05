7f502726304807378519a95698a6e68f0200cada
Dominic Meddick <dominicmeddick@gmail.com>
2022-01-05 13:48:04 +0000

    Added user model with user test to confirm that the user being created is valid

---
+++ test/models/user_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(name: "Example user", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+end
