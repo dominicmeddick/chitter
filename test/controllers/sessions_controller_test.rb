cbdd78f5f44ff85e12c3ab9d8523aac1100c3b3d
Dominic Meddick <dominicmeddick@gmail.com>
2022-01-14 11:33:03 +0000

    passing test for user login with invalid information. Added the flash.now method which is designed to show flash messages on rendered pages

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test "login with invalid information" do
     get login_path
-    assert_response :success
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
   end
 end
